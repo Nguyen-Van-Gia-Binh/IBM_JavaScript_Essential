494b79ae965861481c06712d484ba20821add760
Nguyen-Van-Gia-Binh <nguyenvangiabinh22072006@gmail.com>
2026-01-29 10:58:03 +0700

    Add README.md file

---
+++ varLetConst.js
@@ -0,0 +1,11 @@
+function show() {
+  var functionVar = "I'm a block-scoped var";
+  let functionLet = "I'm a block-scoped let";
+  const functionConst = "I'm a block-scoped const";
+  console.log(functionVar);
+}
+show();
+console.log("Hello");
+// console.log(functionVar); // Throws ReferenceError
+// console.log(functionLet); // Throws ReferenceError
+// console.log(functionConst); // Throws ReferenceError
