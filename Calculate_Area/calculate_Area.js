4b2fbda9b2be564e4b54436fb7e86613110c162a
Nguyen-Van-Gia-Binh <nguyenvangiabinh22072006@gmail.com>
2026-01-29 11:13:16 +0700

    Solve problem Calculate Area

---
+++ Calculate_Area/calculate_Area.js
@@ -0,0 +1,8 @@
+let length;
+let width;
+function calculateArea() {
+  length = parseFloat(document.getElementById("length").value);
+  width = parseFloat(document.getElementById("width").value);
+  document.getElementById("result").innerHTML =
+    `The result is : ${length * width}`;
+}
